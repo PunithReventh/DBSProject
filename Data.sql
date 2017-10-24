02a23a735032a117740de8300e424156e96f9d43
PunithReventh <punith.rajendran8897@gmail.com>
2017-10-25 00:20:19 +0530

    Add files via upload
    Data file, please add data for staff and faculty :*

---
+++ Data.sql
@@ -0,0 +1,32 @@
+INSERT INTO ROLE VALUES(1, 'Student');
+INSERT INTO ROLE VALUES(2, 'Faculty');
+INSERT INTO ROLE VALUES(3, 'CDC');
+
+INSERT INTO USERLOGIN VALUES('ab11@iitbbs.ac.in','7C6A180B36896A0A8C02787EEAFB0E4C',1);
+INSERT INTO USERLOGIN VALUES('cd12@iitbbs.ac.in','6CB75F652A9B52798EB6CF2201057C73',1);
+INSERT INTO USERLOGIN VALUES('ef13@iitbbs.ac.in','819B0643D6B89DC9B579FDFC9094F28E',1);
+INSERT INTO USERLOGIN VALUES('gh14@iitbbs.ac.in','34CC93ECE0BA9E3F6F235D4AF979B16C',1);
+INSERT INTO USERLOGIN VALUES('ij15@iitbbs.ac.in','DB0EDD04AAAC4506F7EDAB03AC855D56',1);
+INSERT INTO USERLOGIN VALUES('kl16@iitbbs.ac.in','218DD27AEBECCECAE69AD8408D9A36BF',1);
+INSERT INTO USERLOGIN VALUES('mn17@iitbbs.ac.in','00CDB7BB942CF6B290CEB97D6ACA64A3',1);
+INSERT INTO USERLOGIN VALUES('op18@iitbbs.ac.in','B25EF06BE3B6948C0BC431DA46C2C738',1);
+INSERT INTO USERLOGIN VALUES('qr19@iitbbs.ac.in','5D69DD95AC183C9643780ED7027D128A',1);
+INSERT INTO USERLOGIN VALUES('st10@iitbbs.ac.in','305E4F55CE823E111A46A9D500BCB86C',1);
+INSERT INTO USERLOGIN VALUES('modi@iitbbs.ac.in','4EA52CFC6D9DCA175F465B5ABEAD09AD',2);
+INSERT INTO USERLOGIN VALUES('rahul@iitbbs.ac.in','8BEBBF5717906DC99BA44C683409404C',2);
+INSERT INTO USERLOGIN VALUES('amit@iitbbs.ac.in','B318CE4F01DC9D006CBF728C18AC4130',2);
+INSERT INTO USERLOGIN VALUES('bangani@iitbbs.ac.in','2C995198DCAE577AA312AA56D7B1E058',2);
+INSERT INTO USERLOGIN VALUES('nani@iitbbs.ac.in','3A1CC5959B95E9E719158CF833961808',2);
+INSERT INTO USERLOGIN VALUES('tpo@iitbbs.ac.in','E004F40EE25B69DB33EDE240DDA003E7',3);
+INSERT INTO USERLOGIN VALUES('cdc@iitbbs.ac.in','61BF30080AA16A01138B43D6340E3538',3);
+
+INSERT INTO STUDENT VALUES('15CS01001','Abhijeet Bannerjee', 9078724356,'ab11@iitbbs.ac.in','B-624, Mahanadi Hall of Residence','No.1, Akbar Road, New Delhi','Research Intern at IIT Delhi','2015-2019','Computer Science', 8.32);
+INSERT INTO STUDENT VALUES('14ME01002','Chandrika Das', 9878293378,'cd12@iitbbs.ac.in','513, Subarnarekha Hall of Residence','No.2, Pattnaik Road, Bhubaneswar','Research Intern at IIT Bhubaneswar','2014-2018','Mechanical', 8.22);
+INSERT INTO STUDENT VALUES('15MM01003','Emmanuel Fernandes', 9885689535,'ef13@iitbbs.ac.in','B-630, Mahanadi Hall of Residence','No.3, Nizam Road, Hyderabad','Research Intern at IIT Hyderabad','2015-2019','Metallurgy', 8.43);
+INSERT INTO STUDENT VALUES('15CE01004','Goutham Handa', 9078648356,'gh14@iitbbs.ac.in','B-701, Mahanadi Hall of Residence','No.4, Sachin Road, Mumbai','Research Intern at IIT Bombay','2015-2019','Civil', 7.78);
+INSERT INTO STUDENT VALUES('14EE01005','Inzamam Jaffar', 9445987474 ,'ij15@iitbbs.ac.in','B-742, Mahanadi Hall of Residence','No.5, Ganguly Road, Kolkata','Research Intern at IIT Kharagpur','2014-2018','Electrical', 9.22);
+INSERT INTO STUDENT VALUES('14EE01006','Kamlesh Lal', 9078363344,'kl16@iitbbs.ac.in','B-744, Mahanadi Hall of Residence','No.6, Yadav Road, Patna','Content Writer at Prasad Solutions','2014-2018','Electrical', 8.75);
+INSERT INTO STUDENT VALUES('14CS01007','Manisha Nath', 7010378398,'mn17@iitbbs.ac.in','405, Subarnarekha Hall of Residence','No.7, Mary Kom Road, Gauhati','Research Intern at IIT Guwahati','2014-2018','Computer Science', 9.5);
+INSERT INTO STUDENT VALUES('14CE01008','Om Prakash', 9884323423,'op18@iitbbs.ac.in','B-211, Mahanadi Hall of Residence','No.8, Mallu Road, Kochi','Analyst at Arab Consultancy','2014-2018','Civil', 7.1);
+INSERT INTO STUDENT VALUES('15ME01009','Qadeem Rizawat', 9444127237,'qr19@iitbbs.ac.in','B-522, Mahanadi Hall of Residence','No.9, Gowda Road, Bengaluru','Research Intern at IISc Bangalore','2015-2019','Mechanical', 7.2);
+INSERT INTO STUDENT VALUES('15EE01010','Sushmita Tiwari', 9176532763,'st10@iitbbs.ac.in','409, Subarnarekha Hall of Residence','No.10, Gandhi Road, Jaipur','Research Intern at IIT Kanpur','2015-2019','Electrical', 8.33);
